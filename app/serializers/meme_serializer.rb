bedc8ca4b7c9a76f8beca1d7638f8722d84ab3c1
asooge <asooge@gmail.com>
2019-10-22 15:23:33 -0400

    Update meme serializer

--- app/serializers/meme_serializer.rb
+++ app/serializers/meme_serializer.rb
@@ -2,4 +2,5 @@ class MemeSerializer < ActiveModel::Serializer
   attributes :id, :image, :text_1, :font_size_1, :text_2, :font_size_2, :comments, :user, :upvotes
   has_one :user_id
   has_many :comments
+  has_many :upvotes
 end
