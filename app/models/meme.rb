5ddec04151ec0e3d658629c0635b0bb3d6436384
asooge <asooge@gmail.com>
2019-10-22 21:55:34 -0400

    Updated serializers

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,5 +1,5 @@
 class Meme < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :upvotes
+  has_many :upvotes, dependent: :destroy
 end
--- app/serializers/user_login_serializer.rb
+++ app/serializers/user_login_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UserLoginSerializer < ActiveModel::Serializer
-  attributes :id, :email, :token, :upvotes
+  attributes :id, :email, :token, :upvotes, :memes
 
   def token
     Rails.application.message_verifier(:signed_token).generate(object.token)
